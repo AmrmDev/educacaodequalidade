c349d2c71eb537e963c88b68bce6c38551154755
Pedro Amorim <amorimpedro23@outlook.com>
2025-05-23 12:47:20 -0300

    Adição do lombok.jar para limpar o código (Getters e Setters)

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,10 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Usuario {
     private int id;
     private String nome;
@@ -7,45 +12,4 @@ public class Usuario {
     private String senha;
     private String telefone;
     private String tipo; // aqui seria tipo, se for calouro, veterano e tal.
-
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-    public void setSenha(String senha) {
-        this.senha = senha;
-   }
-   public String getTelefone() {
-        return telefone;
-   }
-   public void setTelefone(String telefone) {
-        this.telefone = telefone;
-   }
-   public String getTipo() {
-        return tipo;
-   }
-   public void setTipo(String tipo) {
-        this.tipo = tipo;
-   }
 }
