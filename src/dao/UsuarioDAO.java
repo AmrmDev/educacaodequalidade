f3e689701b9e13dbcada8f6bb7464af474ee776f
Pedro Amorim <amorimpedro23@outlook.com>
2025-05-23 13:17:36 -0300

    Criação do CRUD (por enquanto apenas o inserir usuário

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -1,4 +1,29 @@
 package dao;
 
+import model.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class UsuarioDAO {
+    public void inserir(Usuario usuario) {
+        String sql = "INSERT INTO usuarios (nome, email, senha, telefone, tipo) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getSenha());
+            stmt.setString(4, usuario.getTelefone());
+            stmt.setString(5, usuario.getTipo());
+
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
