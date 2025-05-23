a9776fb4baad694e4512490f81400429500e6dd9
Pedro Amorim <amorimpedro23@outlook.com>
2025-05-23 12:42:16 -0300

    Adicionado conexão com o MySQL no arquivo src/dao/Conexao.java

--- src/dao/Conexao.java
+++ src/dao/Conexao.java
@@ -1,4 +1,20 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexao {
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://localhost:3306/rede_educacional";
+            String user = "root";
+            String password = "senha"; // tem q criar a senha aqui
+
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
