8babd9d613fcf749089c40bca880a85f0bcd568e
Pedro Amorim <amorimpedro23@outlook.com>
2025-05-23 13:31:58 -0300

    CriaçÃão do Update | List | Create do CRUD

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import com.sun.security.auth.UnixNumericUserPrincipal;
 import model.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -26,4 +27,52 @@ public class UsuarioDAO {
             e.printStackTrace();
         }
     }
+
+    public List<Usuario> listarTodos() {
+        List<Usuario> lista = new ArrayList<>();
+        String sql = "SELECT * from usuarios";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id")); // Supondo que a tabela tenha o campo "id"
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));
+                usuario.setTelefone(rs.getString("telefone"));
+                usuario.setTipo(rs.getString("tipo"));
+
+                lista.add(usuario);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public void atualizar(Usuario usuario) {
+        String sql = "UPDATE usuarios SET nome = ?, email = ?. senha = ?, telefone = ?, tipo = ? WHERE id = ?";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setString(3, usuario.getSenha());
+            stmt.setString(4, usuario.getTelefone());
+            stmt.setString(5, usuario.getTipo());
+            stmt.setInt(6, usuario.getId());
+
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
+
+
