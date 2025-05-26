61402a2170af9fa0599fb152e01027eb5362990d
Pedro Amorim <amorimpedro23@outlook.com>
2025-05-26 08:11:09 -0300

    Mock de usuário feito | Criação da classe Status em src/util

--- src/dao/UsuarioMockDAO.java
+++ src/dao/UsuarioMockDAO.java
@@ -1,5 +1,35 @@
 package dao;
 
-public class UsuarioMockDAO {
+import model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 
+public class UsuarioMockDAO {
+    private List<Usuario> usuarios = new ArrayList<>();
+    private int idAtual = 1;
+
+    public void inserir(Usuario usuario) {
+        usuario.setId(idAtual++);
+        usuarios.add(usuario);
+        System.out.println("Inserido (Mock)> " + usuario);
+    }
+
+    public List<Usuario> listar() {
+        return usuarios;
+    }
+
+    public void atualizar(Usuario usuario) {
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i).getId() == usuario.getId()) {
+                usuarios.set(i, usuario);
+                System.out.println("Atualizado (Mock): " + usuario);
+                return;
+            }
+        }
+    }
+
+    public void excluir(int id) {
+        usuarios.removeIf(u -> u.getId() == id);
+        System.out.println("Deletado (MOCK), ID: " + id);
+    }
 }
