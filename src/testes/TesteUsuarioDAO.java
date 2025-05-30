10b42f4785ff2b2c1d5f4d7f546e3b80dd16ea0d
Pedro Amorim <amorimpedro23@outlook.com>
2025-05-30 17:15:31 -0300

    Adicionando tratamento de exceções nos testes

--- src/testes/TesteUsuarioDAO.java
+++ src/testes/TesteUsuarioDAO.java
@@ -2,30 +2,37 @@ package testes;
 
 import dao.UsuarioDAO;
 import model.Usuario;
+import util.TipoUsuario;
 
 public class TesteUsuarioDAO {
     public static void main(String[] args) {
-        UsuarioDAO dao =  new UsuarioDAO();
 
-        Usuario usuario = new Usuario();
-        usuario.setNome("Teste");
-        usuario.setEmail("teste@gmail.com");
-        usuario.setSenha("123456");
-        usuario.setTelefone("9 9999-9999");
-        usuario.setTipo("aluno");
+        try {
+            UsuarioDAO dao = new UsuarioDAO();
 
-        dao.inserir(usuario);
-        System.out.println("Usuário inserido com sucesso");
+            Usuario usuario = new Usuario();
+            usuario.setNome("Teste");
+            usuario.setEmail("teste@gmail.com");
+            usuario.setSenha("123456");
+            usuario.setTelefone("9 9999-9999");
+            usuario.setTipo(TipoUsuario.CALOURO);
 
-        for (Usuario u : dao.listarTodos()) {
-            System.out.println(u);
+            dao.inserir(usuario);
+            System.out.println("Usuário inserido com sucesso");
+
+            for (Usuario u : dao.listarTodos()) {
+                System.out.println(u);
+            }
+
+            usuario.setNome("Teste da Silva");
+            dao.atualizar(usuario);
+            System.out.println("Usuário atualizado com sucesso");
+
+            dao.deletar(usuario.getId());
+            System.out.println("Usuário delteado com sucesso");
+
+        } catch(Exception e) {
+            System.err.println("Ocorreu um erro: " + e.getMessage());
         }
-
-        usuario.setNome("Teste da Silva");
-        dao.atualizar(usuario);
-        System.out.println("Usuário atualizado com sucesso");
-
-        dao.deletar(usuario.getId());
-        System.out.println("Usuário delteado com sucesso");
     }
 }
